d199b7ef6ad952c8e7dc515f713bd4dac58a35b4
N.V.CRISTEA <contact@cristea.me>
2012-07-11 23:36:38 +0300

    upgrade to symfony 2.1.0-BETA2

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -13,14 +13,12 @@ class AppKernel extends Kernel
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-            new Symfony\Bundle\DoctrineBundle\DoctrineBundle(),
-            new Symfony\Bundle\DoctrineMigrationsBundle\DoctrineMigrationsBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new JMS\AopBundle\JMSAopBundle(),
+            new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
-            new Acme\HelloBundle\AcmeHelloBundle(),
-            new Acme\StoreBundle\AcmeStoreBundle(),
-            new Users\UsersBundle\UsersUsersBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
