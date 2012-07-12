4dd153a892e3aac6e7eef955bdba5cce5c055867
N.V.CRISTEA <contact@cristea.me>
2012-07-12 03:40:46 +0300

    DoctrineFixturesBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new Pam\AccountBundle\PamAccountBundle(),
         );
 
